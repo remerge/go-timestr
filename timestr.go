50683df2fbe9c4639a79dcbc0c6622ef4b4343b6
Alexander Dorofeev <aka.spin@gmail.com>
2020-06-12 06:30:24 +0200

    use constants for time formats

--- timestr.go
+++ timestr.go
@@ -2,6 +2,11 @@ package timestr
 
 import "time"
 
+const (
+	URLSafeTimeFormat = "2006-01-02T15-04-05Z0700"
+	ISO8601TimeFormat = "2006-01-02T15:04:05Z07:00"
+)
+
 var timeNow time.Time
 var timeNowUTC time.Time
 var timeToday time.Time
@@ -19,8 +24,8 @@ func SetTimeStr(t time.Time) {
 	timeNowUTC = t.UTC()
 	d := time.Duration(-t.Hour()) * time.Hour
 	timeToday = t.Truncate(time.Hour).Add(d)
-	timeStrISO8601 = t.Format("2006-01-02T15:04:05Z07:00")
-	timeStrISO8601inUTC = timeNowUTC.Format("2006-01-02T15:04:05Z07:00")
-	timeStrURLSafe = t.Format("2006-01-02T15-04-05Z0700")
-	timeStrURLSafeinUTC = timeNowUTC.Format("2006-01-02T15-04-05Z0700")
+	timeStrISO8601 = t.Format(ISO8601TimeFormat)
+	timeStrISO8601inUTC = timeNowUTC.Format(ISO8601TimeFormat)
+	timeStrURLSafe = t.Format(URLSafeTimeFormat)
+	timeStrURLSafeinUTC = timeNowUTC.Format(URLSafeTimeFormat)
 }
